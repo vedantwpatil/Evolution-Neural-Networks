af7e7851a5368e686f0fe5f3234a4de70294608c
Vedant Patil <vedant.waman.patil@gmail.com>
2025-02-08 11:23:14 -0500

    Correct project structuring

---
+++ target/debug/deps/lib_neural_network-e4782a090dbffe80.d
@@ -0,0 +1,9 @@
+/Users/vedantpatil/Library/CloudStorage/OneDrive-DrexelUniversity/Documents/cs/personal/projects/rust/evolution/target/debug/deps/liblib_neural_network-e4782a090dbffe80.rmeta: libs/neural-network/src/lib.rs Cargo.toml
+
+/Users/vedantpatil/Library/CloudStorage/OneDrive-DrexelUniversity/Documents/cs/personal/projects/rust/evolution/target/debug/deps/lib_neural_network-e4782a090dbffe80.d: libs/neural-network/src/lib.rs Cargo.toml
+
+libs/neural-network/src/lib.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
